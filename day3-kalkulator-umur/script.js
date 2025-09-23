5f5a954adcd28fef13e37171e240be1694d7586a
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-24 05:51:56 +0700

    fixing: some minor bugs

--- day3-kalkulator-umur/script.js
+++ day3-kalkulator-umur/script.js
@@ -12,11 +12,18 @@ submit.addEventListener("click", function () {
   let bulan = hariIni.getMonth() - lahir.getMonth();
   let hari = hariIni.getDate() - lahir.getDate();
 
-  // jika hasil dari variabel bulan negatif atau 0 dan hari negatif maka kurangi tahun 1
+  // jika tanggal lahir belum tercapai, kurangi tahun
   if (bulan < 0 || (bulan === 0 && hari < 0)) {
     tahun--;
+  } else if (tahun < 0 || isNaN(lahir)) {
+    tahun = "Invalid";
   }
 
   // output
   umur.innerHTML = tahun;
+});
+
+clear.addEventListener("click", function () {
+  tanggalLahir.value = null;
+  umur.innerHTML = "0";
 });
