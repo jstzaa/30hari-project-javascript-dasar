1c5c22bab0cde24a8c65e1320c98fb4252c18cfb
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-28 22:48:36 +0700

    refactoring: penghitung nilai rata-rata

--- day7-penghitung-nilai-rata-rata/script.js
+++ day7-penghitung-nilai-rata-rata/script.js
@@ -1,42 +1,35 @@
-const nilai1 = document.getElementById("nilai1");
-const nilai2 = document.getElementById("nilai2");
-const nilai3 = document.getElementById("nilai3");
+// ambil data
+const grade = document.getElementsByTagName("input");
 const hitung = document.getElementById("hitung");
 const reset = document.getElementById("reset");
 const hasilRataRata = document.getElementById("hasilRataRata");
 
+// operasi jika di klik
 hitung.addEventListener("click", function () {
-  let n1 = parseFloat(nilai1.value);
-  let n2 = parseFloat(nilai2.value);
-  let n3 = parseFloat(nilai3.value);
-  let avg = (n1 + n2 + n3) / 3;
+  for (let i = 0; i < grade.length; i++) {
+    let nilai = parseFloat(grade[i].value);
 
-  //validasi input
-  if (
-    isNaN(n1) ||
-    isNaN(n2) ||
-    isNaN(n3) ||
-    n1 < 0 ||
-    n2 < 0 ||
-    n3 < 0 ||
-    n1 > 100 ||
-    n2 > 100 ||
-    n3 > 100
-  ) {
-    alert("Masukkan semua nilai dengan benar!");
-    nilai1.value = "";
-    nilai2.value = "";
-    nilai3.value = "";
-    hasilRataRata.innerHTML = "";
-    return;
-  } else {
-    hasilRataRata.innerHTML = avg.toFixed(2);
+    // validasi input
+    if (isNaN(nilai) || nilai < 0 || nilai > 100) {
+      alert("Input salah! silakan ulangi");
+      break;
+    } else {
+      // proses
+      let sum =
+        parseFloat(grade[0].value) +
+        parseFloat(grade[1].value) +
+        parseFloat(grade[2].value);
+      let avg = sum / grade.length;
+      // output
+      hasilRataRata.innerHTML = avg.toFixed(2);
+    }
   }
 });
 
+// tombol reset
 reset.addEventListener("click", function () {
-  nilai1.value = "";
-  nilai2.value = "";
-  nilai3.value = "";
+  for (let j = 0; j < grade.length; j++) {
+    grade[j].value = "";
+  }
   hasilRataRata.innerHTML = "";
 });
